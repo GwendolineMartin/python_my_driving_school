18a5f4def96b5967dd96592460d5ead37b7b05e4
Aurelie <durso.aurelie@gmail.com>
2020-04-17 16:07:14 +0200

    update

--- mydjango/intra/urls.py
+++ mydjango/intra/urls.py
@@ -8,7 +8,18 @@ urlpatterns = [
     path('rdv/create/<int:pk>', views.rdv_detail, name='rdv_dedirectTo'),
 
     path('rdv/<int:pk>/edit/', views.rdv_edit, name='rdv_edit'),
+<<<<<<< Updated upstream
+    path('rdv/<int:pk>/remove/', views.rdv_remove, name='rdv_remove')
+=======
     path('rdv/<int:pk>/remove/', views.rdv_remove, name='rdv_remove'),
 
+    path('forfait/<int:pk>/', views.forfait_detail, name='forfait_detail'),
+    path('forfait/create/', views.forfait_create, name='forfait_create'),
+    path('forfait/create/<int:pk>', views.forfait_detail,
+         name='forfait_dedirectTo'),
 
+    path('forfait/<int:pk>/edit/', views.forfait_edit, name='forfait_edit'),
+    path('forfait/<int:pk>/remove/', views.forfait_remove, name='forfait_remove'),
+
+>>>>>>> Stashed changes
 ]
